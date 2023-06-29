ad6082921d834d56f463c92cbfb4ebea3a4232f3
leeturner <lee@leeturner.me>
2023-06-29 14:57:04 +0100

    updated comments re byte array

--- src/main/kotlin/com/leeturner/bufferedimage/BufferedImageController.kt
+++ src/main/kotlin/com/leeturner/bufferedimage/BufferedImageController.kt
@@ -8,7 +8,8 @@ import io.micronaut.http.annotation.Produces
 @Controller("/buffered-image")
 class BufferedImageController {
 
-  // create a controller method that will return a buffered image
+  // create a controller method that will return a byte array, turns out we don't need to return a 
+  // BufferedImage.
   @Get
   @Produces(MediaType.IMAGE_JPEG) // Adjust the media type based on the image format
   fun loadImage(): ByteArray {
